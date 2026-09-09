078786b4443e13ddfb33880d914fae288f9934b2
Chaitanya <chaitanyadirisla@gmail.com>
2026-09-09 18:15:03 +0530

    day-7

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/first_program.py
@@ -0,0 +1,13 @@
+print("This is my first program")
+
+print("your name : Chaitanya")
+print("your age : 22")
+print("city : bangalore")
+print("your favorite programming language : python")
+print("The message : I learned basics of programming and also i learned how to solve the pseudocode problems with the better understanding")
+
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/pseudo.py
@@ -0,0 +1,34 @@
+# To print Hello World
+
+print("Hello World")
+
+# To find wheather number (n) is even or odd
+num = 10
+if num % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+# To find the number is pos, neg, or zero
+num = 10
+if num >= 0:
+    print("positive")
+else:
+    print("negative")
+
+# To find the largest among 3 numbers a, b, c
+a = int(input("enter a : "))
+b = int(input("enter b : "))
+c = int(input("enter c : "))
+if a>b and a>c:
+    print("a is greater")
+elif b>a and b>c:
+    print("b is greater")
+else:
+    print("c is greater")
+
+
+print("Hello World \n Thank you for learning python")
+
+
+
