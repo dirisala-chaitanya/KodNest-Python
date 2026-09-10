bb6b69f38c5ff04e8bab3ffb558584f6f07ce8ac
Chaitanya <chaitanyadirisla@gmail.com>
2026-09-10 14:54:17 +0530

    day-8

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/variable.py
@@ -0,0 +1,21 @@
+name = "Chaitanya"
+name = "Pinky"
+print(name)
+
+age = 22
+# My name is Pinky and age is 22!
+print("My name is " + name)
+print("My name is " + name + " and age is" , age , "!")
+print("My name is " + name + " and age is" + str(age) + "!")
+
+marks = 50
+marks = marks + 10
+print(marks)
+
+
+a , b, c = 10, 20, 30
+print(a, b, c)
+
+x = y = z = 100
+print(x, y, z)
+
