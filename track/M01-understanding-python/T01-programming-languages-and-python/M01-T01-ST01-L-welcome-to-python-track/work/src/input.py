452eddc9ba92e40e46159cab2f0453beb837188d
Chaitanya <chaitanyadirisla@gmail.com>
2026-09-11 14:46:04 +0530

    day-9

---
+++ track/M01-understanding-python/T01-programming-languages-and-python/M01-T01-ST01-L-welcome-to-python-track/work/src/input.py
@@ -0,0 +1,21 @@
+name = "Chaitanya"
+name = input("Enter the name :")
+print(f"My name is{name} !")
+
+age = input("Enter the age")
+print(f"My age is {age}")
+
+# value input -> str
+# add 2 numbers
+a = input("a :")
+b = input("b :")
+c = a + b
+print(c)
+
+a = int(input("a :"))
+b = int(input("b :"))
+c = a + b
+print(c)
+
+
+
